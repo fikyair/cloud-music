188d2458c78bad4e3944253a409b36507783a00c
fikyair <shiming_xsm@163.com>
2020-01-19 11:10:29 +0800

    Scroll 组件

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Slider from '../../components/slider';
 import RecommendList from '../../components/list/index';
+import Scroll from '../../baseUI/scroll';
+import { Content } from './style';
 
 function Recommend() {
 
@@ -19,11 +21,15 @@ function Recommend() {
     });
 
     return (
-        <div>
-            <Slider bannerList={bannerList}></Slider>
-            <RecommendList recommendList={recommendList}>
-            </RecommendList>
-        </div>
+        <Content>
+            <Scroll className="list">
+                <div>
+                    <Slider bannerList={bannerList}></Slider>
+                    <RecommendList recommendList={recommendList}>
+                    </RecommendList>
+                </div>
+            </Scroll>
+        </Content>
     )
 }
 
