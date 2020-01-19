8241a21f21e911c9a725810be362babbc30ad29b
fikyair <shiming_xsm@163.com>
2020-01-19 12:09:15 +0800

    推荐模块

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -6,15 +6,22 @@ import { Content } from './style';
 import * as actionTypes from './store/actionCreators';
 import { connect } from "react-redux";
 import { forceCheck } from 'react-lazyload';
+import Loading from '../../baseUI/loading/index';
 
 
 function Recommend(props) {
     const { bannerList, recommendList } = props;
-    const { getBannerDataDispatch, getRecommendListDataDispatch } = props;
+    const { getBannerDataDispatch, getRecommendListDataDispatch, enterLoading } = props;
 
     useEffect(() => {
-        getBannerDataDispatch();
-        getRecommendListDataDispatch();
+        // 如果页面有数据，则不发请求
+        //immutable 数据结构中长度属性 size
+        if (!bannerList.size) {
+            getBannerDataDispatch();
+        }
+        if (!recommendList.size) {
+            getRecommendListDataDispatch();
+        }
         //eslint-disable-next-line
     }, []);
 
@@ -24,13 +31,14 @@ function Recommend(props) {
 
     return (
         <Content>
-            <Scroll  onScroll={forceCheck}>
+            <Scroll onScroll={forceCheck}>
                 <div>
                     <Slider bannerList={bannerListJS}></Slider>
                     <RecommendList recommendList={recommendListJS}>
                     </RecommendList>
                 </div>
             </Scroll>
+            {enterLoading ? <Loading></Loading> : null}
         </Content>
     )
 }
@@ -41,6 +49,7 @@ const mapStateToProps = (state) => ({
     // 不然每次 diff 比对 props 的时候都是不一样的引用，还是导致不必要的重渲染，属于滥用 immutable
     bannerList: state.getIn(['recommend', 'bannerList']),
     recommendList: state.getIn(['recommend', 'recommendList']),
+    enterLoading: state.getIn(['recommend', 'enterLoading'])
 });
 // 映射 dispatch 到 props 上
 const mapDispatchToProps = (dispatch) => {
